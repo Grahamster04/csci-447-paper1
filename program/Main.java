2e1958a97f2805308d959b717e4d8636fab78c59
Ryan Poffenroth <163058601+AverageTim@users.noreply.github.com>
2026-09-10 11:39:36 -0600

    Added all function method criteria
    Gives a baseline for what functions we need to write

--- program/Main.java
+++ program/Main.java
@@ -3,6 +3,16 @@ public static void main(String[] args) {
   // NULL MODELS
     // Classification: Return the most common data value
     // Regression: Determine the average of each predictor class and return said average
-    
-  
+
+  // K-Nearest Neighbor
+    // Classification
+    // Regression
+
+  // Edited/Condensed K-Nearest Neighbor
+    // Classification
+    // Regression (including ∈ threshold)
+
+  // Class Determinations
+    // Classification (plurality vote)
+    // Regression (Gaussian kernal)
 }
