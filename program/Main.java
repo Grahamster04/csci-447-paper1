874243609692da53ffebea336ce4c22b1c6a909a
Ryan Poffenroth <163058601+AverageTim@users.noreply.github.com>
2026-09-10 10:31:13 -0600

    Added basic null model criteria
    Includes comments on how to create the null models

--- program/Main.java
+++ program/Main.java
@@ -1,3 +1,8 @@
 public static void main(String[] args) {
 
+  // NULL MODELS
+    // Classification: Return the most common data value
+    // Regression: Determine the average of each predictor class and return said average
+    
+  
 }
