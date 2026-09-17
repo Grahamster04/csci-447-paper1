d7c1edea868e453200cf5a57bcfd89edeba2347e
AverageTim <ryan.poffenroth@icloud.com>
2026-09-16 18:32:26 -0600

    added/fixed normalization

--- program/Machine.java
+++ program/Machine.java
@@ -3,18 +3,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class Machine {
+public class Machine implements normalizedFeatures{
 
     static ArrayList<Machine> hardwareStorage = new ArrayList<>();
 
-    public int MYCT; public static int MYCTMax;
-    public int MMIN; public static int MMINMax;
-    public int MMAX; public static int MMAXMax;
-    public int CACH; public static int CACHMax;
-    public int CHMIN; public static int CHMINMax;
-    public int CHMAX; public static int CHMAXMax;
-    public int PRP; public static int PRPMax;
-    public int ERP; public static int ERPMax;
+    public int MYCT; public static int MYCTMax; public static int MYCTMin;
+    public int MMIN; public static int MMINMax; public static int MMINMin;
+    public int MMAX; public static int MMAXMax; public static int MMAXMin;
+    public int CACH; public static int CACHMax; public static int CACHMin;
+    public int CHMIN; public static int CHMINMax; public static int CHMINMin;
+    public int CHMAX; public static int CHMAXMax; public static int CHMAXMin;
+    public int PRP;
+    public int ERP; public static int ERPMax; public static int ERPMin;
 
     public Machine(int mcyt, int mmin, int mmax, int cash, int chmin, int chmax, int prp, int erp) {
         this.MYCT = mcyt;
@@ -44,14 +44,13 @@ public class Machine {
                 );
 
                 // Normalization for machine
-                if (temp.MYCT > MYCTMax) { MYCTMax = temp.MYCT; }
-                if (temp.MMIN > MMINMax) { MMINMax = temp.MMIN; }
-                if (temp.MMAX > MMAXMax) { MMAXMax = temp.MMAX; }
-                if (temp.CACH > CACHMax) { CACHMax = temp.CACH; }
-                if (temp.CHMIN > CHMINMax) { CHMINMax = temp.CHMIN; }
-                if (temp.CHMAX > CHMAXMax) { CHMAXMax = temp.CHMAX; }
-                if (temp.PRP > PRPMax) { PRPMax = temp.PRP; }
-                if (temp.ERP > ERPMax) { ERPMax = temp.ERP; }
+                if (temp.MYCT > MYCTMax) { MYCTMax = temp.MYCT; } if (temp.MYCT < MYCTMin) { MYCTMin = temp.MYCT; }
+                if (temp.MMIN > MMINMax) { MMINMax = temp.MMIN; } if (temp.MMIN < MMINMin) { MMINMin = temp.MMIN; }
+                if (temp.MMAX > MMAXMax) { MMAXMax = temp.MMAX; } if (temp.MMAX < MMAXMin) { MMAXMin = temp.MMAX; }
+                if (temp.CACH > CACHMax) { CACHMax = temp.CACH; } if (temp.CACH < CACHMin) { CACHMin = temp.CACH; }
+                if (temp.CHMIN > CHMINMax) { CHMINMax = temp.CHMIN; } if (temp.CHMIN < CHMINMin) { CHMINMin = temp.CHMIN; }
+                if (temp.CHMAX > CHMAXMax) { CHMAXMax = temp.CHMAX; } if (temp.CHMAX < CHMAXMin) { MYCTMin = temp.CHMAXMin; }
+                if (temp.ERP > ERPMax) { ERPMax = temp.ERP; } if (temp.ERP < ERPMin) { ERPMin = temp.ERP; }
 
                 hardwareStorage.add(temp);
             }
@@ -69,19 +68,20 @@ public class Machine {
                 encodeCACH(),
                 encodeCHMIN(),
                 encodeCHMAX(),
-                encodePRP(),
                 encodeERP()
         };
     }
 
-    private double encodeMYCT() { return (double) MYCT / MYCTMax; }
-    private double encodeMMIN() { return (double) MMIN / MMINMax; }
-    private double encodeMMAX() { return (double) MMAX / MMAXMax; }
-    private double encodeCACH() { return (double) CACH / CACHMax; }
-    private double encodeCHMIN() { return (double) CHMIN / CHMINMax; }
-    private double encodeCHMAX() { return (double) CHMAX / CHMAXMax; }
-    private double encodePRP() { return (double) PRP / PRPMax; }
-    private double encodeERP() { return (double) ERP / ERPMax; }
+    @Override
+    public double getLable() { return PRP; }
+
+    private double encodeMYCT() { return (double) (MYCT - MYCTMin) / (MYCTMax - MYCTMin); }
+    private double encodeMMIN() { return (double) (MMIN - MMINMin) / (MMINMax - MMINMin); }
+    private double encodeMMAX() { return (double) (MMAX - MMAXMin) / (MMAXMax - MMAXMin); }
+    private double encodeCACH() { return (double) (CACH - CACHMin) / (CACHMax - CACHMin); }
+    private double encodeCHMIN() { return (double) (CHMIN - CHMINMin) / (CHMINMax - CHMINMin); }
+    private double encodeCHMAX() { return (double) (CHMAX - CHMAXMax) / (CHMAXMax - CHMAXMin); }
+    private double encodeERP() { return (double) (ERP - ERPMin) / (ERPMax - ERPMin); }
 
     public static void classify() {
         if (hardwareStorage.isEmpty()) {
